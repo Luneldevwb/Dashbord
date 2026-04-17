e17b3908184636d5c880e9dfc17b784a3b667dd1
Luneldevwb <nematt007@gmail.com>
2026-04-18 00:57:21 +0300

    added js for toggle

--- app/js/script.js
+++ app/js/script.js
@@ -1,2 +1,9 @@
-var num = 'hello';
-alert(num);
+// const radioButtons = document.querySelectorAll('.toggle__wrapper input');
+// for(let i = 0; i < radioButtons.length; i++){
+// 	radioButtons[i].addEventListener('click', Event => {
+// 		document.getElementById('dark').checked 
+// 		  ?(document.querySelector('body').classList = 'dark')
+// 		  :(document.querySelector('body').classList = 'light');
+// 	});
+
+// }
