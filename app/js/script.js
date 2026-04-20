53a55b8dafb5f28596938f6cc139f5cab7f1fe09
Luneldevwb <nematt007@gmail.com>
2026-04-21 00:31:05 +0300

    New mod js.

--- app/js/script.js
+++ app/js/script.js
@@ -1,9 +1,10 @@
 const radioButtons = document.querySelectorAll('.toggle__wrapper input');
 for(let i = 0; i < radioButtons.length; i++){
 	radioButtons[i].addEventListener('click', Event => {
-		document.getElementById('dark').checked 
-		  ?(document.querySelector('body').classList = 'dark')
-		  :(document.querySelector('body').classList = 'light');
+		if (document.getElementById('dark').checked) {
+			(document.querySelector('body').classList = 'dark');
+		} 
+		else {document.querySelector('body').classList = 'light';}
 	});
 
 }
