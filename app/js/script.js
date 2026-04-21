64632da589fd59e44255a22b7d7ca9299181feca
Luneldevwb <nematt007@gmail.com>
2026-04-22 00:26:27 +0300

    new mod for the script.

--- app/js/script.js
+++ app/js/script.js
@@ -3,8 +3,11 @@ for(let i = 0; i < radioButtons.length; i++){
 	radioButtons[i].addEventListener('click', Event => {
 		if (document.getElementById('dark').checked) {
 			(document.querySelector('body').classList = 'dark');
+			localStorage.setItem('colorMode', 'dark');
 		} 
-		else {document.querySelector('body').classList = 'light';}
+		else {document.querySelector('body').classList = 'light';
+			localStorage.setItem('colorMode', 'light');
+		}
 	});
 
 }
