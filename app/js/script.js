6d9e3dba8da6787cde12922cc25ae64763ba7d7f
Luneldevwb <nematt007@gmail.com>
2026-04-23 00:10:11 +0300

    Created fn and var for dark and light.

--- app/js/script.js
+++ app/js/script.js
@@ -1,12 +1,25 @@
+const getColorMode = () => {
+	localStorage.getItem('ColorMode');
+};
+
+const setDarkMode = () => {
+	document.querySelector('body').classList = 'dark';
+};
+
+const setLightMode = () => {
+	document.querySelector('body').classList = 'light';
+};
+
 const radioButtons = document.querySelectorAll('.toggle__wrapper input');
 for(let i = 0; i < radioButtons.length; i++){
 	radioButtons[i].addEventListener('click', Event => {
 		if (document.getElementById('dark').checked) {
-			(document.querySelector('body').classList = 'dark');
 			localStorage.setItem('colorMode', 'dark');
+			setDarkMode();
 		} 
-		else {document.querySelector('body').classList = 'light';
+		else {
 			localStorage.setItem('colorMode', 'light');
+			setLightMode();
 		}
 	});
 
